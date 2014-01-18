38c3fa3970dbf608b0d4d3bb31504084915181c9
James Scott <jim.scott@urbanairship.com>
2014-01-18 14:28:27 -0800

    Bad PR including terrible python code from another project.

---
+++ file1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+# EXAMPLE SHITTY PYTHON
+#
+#
+
+
+def main():
+    print "PYTHON IS A PRETTY COOL GUY"
+
+
+if __name__ == '__main__':
+    b = None
+    if b is not None:
+		print "THIS LINE HAS BAD tabs"
+        print b
+    print "THIS IS SYNTAX ERROR"
+    main(b)
+
+    exit(1)
+
